6bd6d2b752720c619dc4a885bc2a76f5b6b21b02
Antony.Glim <Antony.Glim@gmail.com>
2019-04-20 22:47:23 +0300

    CommandCONTENT add

--- Client/src/main/java/command/CommandCONTENT.java
+++ Client/src/main/java/command/CommandCONTENT.java
@@ -1,7 +1,25 @@
 package command;
 
+import client.FilesList;
+import common.ConsoleHelper;
+import exception.PathIsNotFoundException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class CommandCONTENT extends CommandClientOnly {
     public void execute() throws Exception {
+        try {
+            ConsoleHelper.writeMessage("Просмотр списока файлов, для отправки на сервер.");
 
+            FilesList filesList = getFilesList();
+
+            for (Path path : filesList.getFileList()) {
+                ConsoleHelper.writeMessage(path.toString());
+            }
+
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Файл не был найден.");
+        }
     }
 }
