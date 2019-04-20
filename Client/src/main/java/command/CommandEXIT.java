5d780353477d1823d9adbf9be34575e39cecc961
Antony.Glim <Antony.Glim@gmail.com>
2019-04-20 21:23:29 +0300

    CommandEXIT done!

--- Client/src/main/java/command/CommandEXIT.java
+++ Client/src/main/java/command/CommandEXIT.java
@@ -1,7 +1,12 @@
 package command;
 
-public class CommandEXIT extends CommandClientOnly {
-    public void execute() throws Exception {
+import common.ConsoleHelper;
 
+/**
+ * The command is executed at the end of the program.
+ */
+public class CommandEXIT implements Command {
+    public void execute() throws Exception {
+        ConsoleHelper.writeMessage("До встречи!");
     }
 }
