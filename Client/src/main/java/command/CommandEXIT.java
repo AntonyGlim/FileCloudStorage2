a625742ae94f991c57605a317fa8e0f8d6e93a40
Antony.Glim <Antony.Glim@gmail.com>
2019-04-22 21:45:40 +0300

    saving data to the database on exit. In the process

--- Client/src/main/java/command/CommandEXIT.java
+++ Client/src/main/java/command/CommandEXIT.java
@@ -1,13 +1,23 @@
 package command;
 
+import client.DBManager;
 import common.ConsoleHelper;
+
 
 /**
  * The command is executed at the end of the program.
  */
 public class CommandEXIT implements Command {
+
+    private static String tableName = "fileslist";
+    DBManager dbManager = new DBManager();
+
     public void execute() throws Exception {
-        //TODO save information
+
+        try {
+            dbManager.connect();
+            dbManager.deleteAllFromTable(tableName);
+        }
         ConsoleHelper.writeMessage("До встречи!");
     }
 }
