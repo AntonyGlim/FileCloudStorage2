7e433f50463fb8529f6e7f5871cdb4dc409ef4a9
Antony.Glim <Antony.Glim@gmail.com>
2019-04-22 23:15:53 +0300

    check if there in no data in user list - and nothing to write on to DB

--- Client/src/main/java/command/CommandEXIT.java
+++ Client/src/main/java/command/CommandEXIT.java
@@ -23,23 +23,24 @@ public class CommandEXIT extends CommandClientOnly {
             DBManager dbManager = new DBManager();
             try {
                 dbManager.connect();
-                dbManager.deleteAllFromTable(tableName);
-
-                for (FileProperties file : filesList.getFileList()) {
-                    dbManager.insertIntoTable(
-                            tableName,
-                            file.getName().toString(),
-                            file.getSize(),
-                            file.getAbsolutePath().toString(),
-                            file.getTimeWhenAdd().getTime()
-                            );
+                dbManager.deleteAllFromTable(tableName); //перед каждым сохранением удаляем все из БД
+                if (filesList.size() > 0){
+                    for (FileProperties file : filesList.getFileList()) {
+                        dbManager.insertIntoTable(
+                                tableName,
+                                file.getName(),
+                                file.getSize(),
+                                file.getAbsolutePath().toString(),
+                                file.getTimeWhenAdd().getTime()
+                        );
+                    }
+                    ConsoleHelper.writeMessage("Список файлов сохранен.");
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
                 ConsoleHelper.writeMessage("Ошибка БД");
             } finally {
                 dbManager.disconnect();
-
             }
         } catch (PathIsNotFoundException e) {
             ConsoleHelper.writeMessage("Файл не был найден.");
