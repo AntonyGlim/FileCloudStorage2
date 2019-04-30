8f521898161d4abf67d65d7d33e1eee9538bd917
Antony.Glim@gmail.com <Antony.Glim@gmail.com>
2019-04-30 17:17:20 +0300

    enum MessageType added

--- Common/src/main/java/common/common/MessageType.java
+++ Common/src/main/java/common/common/MessageType.java
@@ -1,4 +1,15 @@
 package common.common;
-
-public class MessageType {
+/**
+ * Enum отвечает за тип сообщений пересылаемых между
+ * клиентом и сервером.
+ */
+public enum MessageType {
+    NAME_REQUEST,       //запрос имени.
+    USER_NAME,          //имя пользователя.
+    USER_PASSWORD,       //пароль пользователя.
+    NAME_ACCEPTED,      //имя принято.
+    USER_ADDED,         //пользователь добавлен.
+    USER_REMOVED,       //пользователь удален.
+    TEXT,               //текстовое сообщение.
+    FILE                //передача файла
 }
