86dca9c3b7eecbf2c126c5f781b33e982884665c
Antony.Glim <Antony.Glim@gmail.com>
2019-05-03 18:03:02 +0300

    adding files is successful

--- Client/src/main/java/command/CommandUPLOAD.java
+++ Client/src/main/java/command/CommandUPLOAD.java
@@ -18,18 +18,19 @@ import java.nio.file.Paths;
 public class CommandUPLOAD extends CommandClientOnly {
     @Override
     public void execute() throws Exception {
-//        try {
+        try {
             ConsoleHelper.writeMessage("Отправка файла на сервер.");
 
-//            ConsoleHelper.writeMessage("Введите полное имя файла для добавления:");
+            ConsoleHelper.writeMessage("Введите полное имя файла для добавления:");
 //
-//            Path sourcePath = Paths.get(ConsoleHelper.readString());
-//            if (Files.notExists(sourcePath)) throw new PathIsNotFoundException();
-            ConnectionManager.getConnectionManager(null).send(new Message(MessageType.FILE, new File("Client/1.jpg")));
+            Path sourcePath = Paths.get(ConsoleHelper.readString());
+            if (Files.notExists(sourcePath)) throw new PathIsNotFoundException();
+            ConnectionManager.getConnectionManager(null).send(new Message(MessageType.FILE, sourcePath.toFile()));
+//            ConnectionManager.getConnectionManager(null).send(new Message(MessageType.FILE, new File("Client/1.jpg")));
             ConsoleHelper.writeMessage("Файла успешно отправлен.");
 
-//        } catch (PathIsNotFoundException e) {
-//            ConsoleHelper.writeMessage("Файл не был найден.");
-//        }
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Файл не был найден.");
+        }
     }
 }
