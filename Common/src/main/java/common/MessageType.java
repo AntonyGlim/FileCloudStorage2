78b2061fa1b5dc15cc99910fce0d834f95652c5d
Antony.Glim <Antony.Glim@gmail.com>
2019-05-07 23:15:30 +0300

    enum MessageType comment and new types add

--- Common/src/main/java/common/MessageType.java
+++ Common/src/main/java/common/MessageType.java
@@ -1,7 +1,8 @@
 package common;
+
 /**
- * Enum отвечает за тип сообщений пересылаемых между
- * клиентом и сервером.
+ * All messages from client to server and backward
+ * must have one of this type
  */
 public enum MessageType {
     AUTHORIZATION,
@@ -9,14 +10,16 @@ public enum MessageType {
     REGISTRATION,
     REGISTRATION_OK,
     DISCONNECTION,
+    DISCONNECTION_OK,
+    FILE,
+    FILE_OK
 
+//    NAME_REQUEST,       //запрос имени.
+//    USER_NAME,          //имя пользователя.
+//    USER_PASSWORD,       //пароль пользователя.
+//    NAME_ACCEPTED,      //имя принято.
+//    USER_ADDED,         //пользователь добавлен.
+//    USER_REMOVED,       //пользователь удален.
+//    TEXT,               //текстовое сообщение.
 
-    NAME_REQUEST,       //запрос имени.
-    USER_NAME,          //имя пользователя.
-    USER_PASSWORD,       //пароль пользователя.
-    NAME_ACCEPTED,      //имя принято.
-    USER_ADDED,         //пользователь добавлен.
-    USER_REMOVED,       //пользователь удален.
-    TEXT,               //текстовое сообщение.
-    FILE,                //передача файла
 }
