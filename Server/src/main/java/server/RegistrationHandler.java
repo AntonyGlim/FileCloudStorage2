65a9ed4947b3582b1b8a21d4bb4b2933ebfe6ccb
Antony.Glim <Antony.Glim@gmail.com>
2019-05-10 12:33:32 +0300

    User user deleted from this class and replace by MainHandler.user too

--- Server/src/main/java/server/RegistrationHandler.java
+++ Server/src/main/java/server/RegistrationHandler.java
@@ -13,7 +13,6 @@ import java.sql.SQLException;
 
 public class RegistrationHandler extends ChannelInboundHandlerAdapter {
 
-    private User user;
     private static DBManager dbManager = new DBManager();
 
     @Override
@@ -32,7 +31,7 @@ public class RegistrationHandler extends ChannelInboundHandlerAdapter {
                         long timeWhenAdd = System.currentTimeMillis();
                         long timeLastChange = timeWhenAdd;
                         dbManager.insertIntoTable(name, password, timeWhenAdd, timeLastChange);
-                        user = dbManager.returnUserFromDBbyNameAndPass(name, password);
+                        MainHandler.user = dbManager.returnUserFromDBbyNameAndPass(name, password);
                         Server.connectionUsersMap.put(name, System.currentTimeMillis());
                         ctx.writeAndFlush(new Message(MessageType.REGISTRATION_OK, "Регистрация выполнена успешно."));
                         ConsoleHelper.writeMessage(Server.connectionUsersMap.toString()); //TODO Delete this
