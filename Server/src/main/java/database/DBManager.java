b3f9c02963a29c98ff7b45de269e31e0855afcc3
Antony.Glim <Antony.Glim@gmail.com>
2019-05-15 19:33:43 +0300

    Put things in order in DBManager class on the server side

--- Server/src/main/java/database/DBManager.java
+++ Server/src/main/java/database/DBManager.java
@@ -1,38 +1,29 @@
 package database;
 
 import common.ConsoleHelper;
-import common.exception.PathIsNotFoundException;
 import exeption.NoSuchUserException;
 import user.User;
 
-import java.nio.file.Paths;
 import java.sql.*;
-import java.util.Date;
-
 
 /**
- * Класс будет отвечать за работу с базой данных
+ * Work with DB.
+ * All public methods has own connect() and disconnect()
  */
 public class DBManager {
     private static Connection connection;
     private static Statement statement;
     private static String tableName = "users";
 
-    /**
-     * Устанавливаем соединение с БД
-     * @throws ClassNotFoundException
-     * @throws SQLException
-     */
-    public static void connect() throws ClassNotFoundException, SQLException {
+    /** Database connection */
+    private static void connect() throws ClassNotFoundException, SQLException {
         Class.forName("org.sqlite.JDBC");
-        connection = DriverManager.getConnection("jdbc:sqlite:Server/src/main/java/database/users.db");
+        connection = DriverManager.getConnection("jdbc:sqlite:Server/src/main/java/database/" + tableName + ".db");
         statement = connection.createStatement();
     }
 
-    /**
-     * Закрываем соединение с БД
-     */
-    public static void disconnect(){
+    /** Database disconnection */
+    private static void disconnect(){
         try {
             connection.close();
         } catch (SQLException e) {
@@ -40,9 +31,7 @@ public class DBManager {
         }
     }
 
-    /**
-     * Метод добавляет запись в таблицу
-     */
+    /** Add an entry to the table */
     public static void insertIntoTable(int name, int password, long timeWhenAdd, long timeLastVisit) throws SQLException {
         try {
             connect();
@@ -56,17 +45,13 @@ public class DBManager {
         }
     }
 
-    /**
-     * Метод вернет FilesListManager, который сформирует из БД.
-     * @throws SQLException
-     */
+    /** Return User, by name and password */
     public static User returnUserFromDBbyNameAndPass(int name, int password) throws SQLException, NoSuchUserException {
         User user = null;
         try {
             connect();
             String sql = String.format("SELECT * FROM %s WHERE name = %d AND password = %d", tableName, name, password);
             ResultSet rs = statement.executeQuery(sql);
-
             if (rs.next()) {
                 user = new User(
                         rs.getInt(2),
@@ -77,29 +62,12 @@ public class DBManager {
             } else {
                 throw new NoSuchUserException();
             }
-
         } catch (ClassNotFoundException e) {
             ConsoleHelper.writeMessage("Ошибка при загрузке данных");
         } finally {
             disconnect();
         }
         return user;
-    }
-
-    /**
-     * Метод удаляет все данные из таблицы.
-     * @throws SQLException
-     */
-    public static void deleteAllFromTable() throws SQLException {
-        try {
-            connect();
-            String sql = String.format("DELETE FROM %s ", tableName);
-            statement.execute(sql);
-        } catch (ClassNotFoundException e) {
-            ConsoleHelper.writeMessage("Ошибка при работе с БД");
-        } finally {
-            disconnect();
-        }
     }
 
 }
